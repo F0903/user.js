0fbdb1888c7f78e986f3f8aff8cd7aff63d1ce3f
Frederik P. <frepbh@gmail.com>
2024-08-28 17:36:17 +0200

    updated overrides

--- user-overrides.js
+++ user-overrides.js
@@ -1,9 +1,12 @@
+// Session restore.
 user_pref("browser.startup.page", 3); //102
 
+// Disable RFP.
 user_pref("privacy.resistFingerprinting", false); // 4501
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 [pointless if not using RFP]
 user_pref("webgl.disabled", false); // 4520 [mostly pointless if not using RFP]
 
+// DNS over HTTPS.
 user_pref("network.trr.mode", 3);
 user_pref("network.trr.bootstrapAddr", "172.64.41.4");
 user_pref("network.trr.url", "https://mozilla.cloudflare-dns.com/dns-query");
@@ -12,21 +15,18 @@ user_pref(
   "https://mozilla.cloudflare-dns.com/dns-query"
 );
 
+// Don't reset everything on close.
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+
 user_pref("browser.formfill.enable", true);
 user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.urlbar.suggest.searches", true);
 user_pref("browser.newtabpage.enabled", true);
 
+// Easier sign-in and such.
 user_pref("signon.autofillForms", true);
 user_pref("signon.formlessCapture.enabled", true);
 user_pref("network.http.windows-sso.enabled", true);
 
-user_pref("browser.cache.disk.enable", true);
+//user_pref("browser.cache.disk.enable", true);
 user_pref("toolkit.winRegisterApplicationRestart", true);
-
-user_pref("privacy.clearOnShutdown.cache", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown_v2.cache", false); // [FF128+] [DEFAULT: true]
-user_pref("privacy.clearOnShutdown.downloads", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown.formdata", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown.history", false); // [DEFAULT: true]
-user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // [FF128+] [DEFAULT: true]
